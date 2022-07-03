3097756ed6751b211c9287506398acaeea3b7129
Roymaniac <amolojoseph2020@gmail.com>
2022-07-03 14:02:45 +0100

    deployment postgres database setup

--- config/settings.py
+++ config/settings.py
@@ -108,14 +108,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-    },
+    'default': env.dj_db_url("DATABASE_URL")
 }
 
 
